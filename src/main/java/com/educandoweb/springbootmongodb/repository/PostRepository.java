43008d7f5404d39fa075b5d515e7aef383d472b0
AlanGustav0 <alangustavo946@gmail.com>
2022-07-26 07:47:52 -0300

    Consulta simples com query methods

--- src/main/java/com/educandoweb/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/educandoweb/springbootmongodb/repository/PostRepository.java
@@ -4,5 +4,9 @@ import com.educandoweb.springbootmongodb.domain.Post;
 import com.educandoweb.springbootmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post,String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/educandoweb/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/educandoweb/springbootmongodb/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.educandoweb.springbootmongodb.resources;
 import com.educandoweb.springbootmongodb.domain.Post;
 import com.educandoweb.springbootmongodb.domain.User;
 import com.educandoweb.springbootmongodb.dto.UserDTO;
+import com.educandoweb.springbootmongodb.resources.util.URL;
 import com.educandoweb.springbootmongodb.services.PostService;
 import com.educandoweb.springbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,6 +27,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 
