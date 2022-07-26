b852d26adce7e1914480dfc7ae13878a10a6e180
AlanGustav0 <alangustavo946@gmail.com>
2022-07-26 08:25:09 -0300

    Consulta com vários critérios

--- src/main/java/com/educandoweb/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/educandoweb/springbootmongodb/repository/PostRepository.java
@@ -5,6 +5,7 @@ import com.educandoweb.springbootmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post,String> {
@@ -13,4 +14,7 @@ public interface PostRepository extends MongoRepository<Post,String> {
     List<Post> findByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [  { date: { $gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ {'title': { $regex: ?0, $options: 'i'} }, {'body': { $regex: ?0, $options: 'i'} }, {'comments.text': { $regex: ?0, $options: 'i'} } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/educandoweb/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/educandoweb/springbootmongodb/resources/PostResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,6 +28,19 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullsearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate
+    ) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate,new Date(0L));
+        Date max = URL.convertDate(maxDate,new Date());
+        List<Post> list = postService.fullSearch(text,min,max);
+        return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/titlesearch")
